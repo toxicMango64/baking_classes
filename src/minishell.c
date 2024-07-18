3fe9880536a2aeb0c6ef8cf9e510e20429230093
Your Name <you@example.com>
2024-07-18 13:43:36 +0400

    skeleton of minishell

---
+++ src/minishell.c
@@ -0,0 +1,43 @@
+#include "../inc/minishell.h"
+
+/**
+ * char *argv[] = {"ls", "-l", "-a", NULL}; // arguments for the new program
+ * char *envp[] = {"PATH=/bin:/usr/bin", "HOME=/home/user", NULL}; // environment variables
+ * if (bultin)
+ * 	exe_bultinft();
+ * else
+ * 	if (execve("/bin/bash", argv, envp) == -1)
+ * 		(perror("execve"), exit(EXIT_FAILURE));
+ */
+int	main(int ac, char **av)
+{
+	char		*user_input;
+	// char		*command;
+
+	if (1 < ac)
+		return (printf("%s: %s: is a file or a directory", av[0], av[1]), 126); // minishell does not take args
+	
+	while (1)
+	{
+		user_input = readline("minishell-v1$ ");
+		if (!user_input)
+			return (perror("user input error"), 126);
+		if (*user_input)
+			add_history(user_input);
+		// command = parse(user_input);
+		printf ("{%s}\n", user_input);
+
+		free (user_input);
+	}
+
+	return (0);
+}
+/**
+ * example for env:
+ * 
+ * /usr/local/bin
+ * /usr/bin
+ * /bin
+ * /usr/sbin
+ * /sbin
+*/
