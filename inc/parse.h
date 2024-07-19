6ed753389746c3ef00a48cb5122c8f67e9169117
Your Name <you@example.com>
2024-07-19 14:19:22 +0400

    parsing now trims the string for furture use before adding to the history

--- inc/parse.h
+++ inc/parse.h
@@ -1,7 +1,11 @@
 #ifndef PARSE_H
 # define PARSE_H
 
+# include "minishell.h"
 # include <string.h>
 # include <stdlib.h>
+
+// char **parse(char *user_input);
+char *parse(char *user_input);
 
 #endif
