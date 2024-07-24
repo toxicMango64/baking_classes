8ec8999d5c06327929573789f7100fa20181087c
mualkhid <162097353+mualkhid@users.noreply.github.com>
2024-07-24 13:34:03 +0000

    w/ nuha's changes

--- inc/parse.h
+++ inc/parse.h
@@ -2,12 +2,10 @@
 # define PARSE_H
 
 # include "minishell.h"
-# include <readline/readline.h>
-# include <readline/history.h>
 # include <string.h>
 # include <stdlib.h>
 
-char	*parse(char *user_input);
+char	**parse(char *user_input);
 int		ft_isspace(int c);
 
 #endif
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,7 +1,6 @@
 #include "../../inc/parse.h"
 #include "../../inc/minishell.h"
 
-
 char	*trimcleanstring(char *str)
 {
 	char *start;
@@ -33,45 +32,46 @@ char	*trimcleanstring(char *str)
 	return (str);
 }
 
-char	*parse(char *user_input)
+char **check_quotes(char **pipe_splitted)
 {
-	char	*clean_input;
-	// char	**command;
-
-	clean_input = trimcleanstring(user_input);
-	if (*user_input || *clean_input)
-		add_history(clean_input);
-	return (clean_input);
-	// return (command);
+	int i = 0;
+	while (pipe_splitted[i])
+	{
+		int j = 0;
+		int in_single_quote = 0;
+		int in_double_quote = 0;
+		while (pipe_splitted[i][j])
+		{
+			if (pipe_splitted[i][j] == '"')
+			{
+				if (!in_single_quote)
+					in_double_quote =!in_double_quote;
+			}
+			else if (pipe_splitted[i][j] == '\'')
+			{
+				if (!in_double_quote)
+					in_single_quote =!in_single_quote;
+			}
+			j++;
+		}
+		if ((in_double_quote) || (in_single_quote))
+			printf("you have unclosed double quote in str[%d]\n", i);
+		i++;
+	}
+	return (pipe_splitted);
 }
+char	**parse(char *user_input)
+{
+	char	*no_spaces_input;
+	char** pipe_splitted;
+	char** checked_quotes;
 
-// char *trimcleanstring(char *str)
-// {
-// 	char *start;
-// 	char *end;
-// 	char *output;
-
-// 	start = str;
-// 	end = str;
-// 	output = str;
-// 	while (ft_isspace((unsigned char)*start))
-// 		start++;
-// 	if (*start == '\0')
-// 		return output;
-// 	while (*end != '\0')
-// 	{
-// 		if (ft_isspace((unsigned char)*end))
-// 		{
-// 			end++;
-// 			if (!ft_isspace((unsigned char)*end) && *end != '\0')
-// 				*output++ = ' ';
-// 		}
-// 		else
-// 			*output++ = *end++;
-// 	}
-// 	if (output > str && ft_isspace((unsigned char)*(output - 1)))
-// 		*(output - 1) = '\0';
-// 	else
-// 		*output = '\0';
-// 	return str;
-// }
+	no_spaces_input = trimcleanstring(user_input);
+	pipe_splitted = ft_split(no_spaces_input, '|');
+	checked_quotes = check_quotes(pipe_splitted);
+	if (*user_input || *no_spaces_input)
+		add_history(user_input);
+	return (pipe_splitted);
+}
+// this function does not handle the follwoing case :
+// ls -la libft | cat input.t"x't"
