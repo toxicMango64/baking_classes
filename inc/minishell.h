a791ee2ad26418eea524b76f0e6e19c30a31fdfd
Your Name <you@example.com>
2024-07-25 11:36:37 +0400

    updated header files

--- inc/minishell.h
+++ inc/minishell.h
@@ -11,11 +11,6 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/libft.h"
-#include "exec.h"
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 
 /* --------------------------------- minishell ------------------------------ */
 
@@ -29,15 +24,79 @@
 
 /* --------------------------------- minishell ------------------------------ */
 
-int				ft_atoi(const char *nptr);
-char			*ft_strdup(const char *s1);
-size_t			ft_strlen(const char *s);
-char			*ft_substr(char const *s, unsigned int start, size_t len);
-void			ft_putchar_fd(char c, int fd);
-void			ft_putstr_fd(char *s, int fd);
+/* --------------------------------- minishell ------------------------------ */
 
+// extern char **environ;
+
+// structs
+typedef enum
+{
+	WORD,
+	OPERATOR,
+	PIPE,
+	/* ... */
+}	token_type;
+
+// Define a t_token structure
+typedef struct s_token
+{
+	token_type		type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_command
+{
+	char				**argv;
+	int					argc;
+	char				*cmd;
+	struct s_command	*next;
+}	t_command;
+
+// typedef struct s_cmdline
+// {
+// 	char				*arguments[MAX_ARGUMENTS];
+// 	int					argcount;
+// 	char const			*input_redirect;
+// 	char const			*output_redirect;
+// 	int					background;
+// 	struct s_cmdline	*next;
+// }	t_cmdline;
+
+typedef struct s_minishell
+{
+	char	**env;
+	char	*pwd;
+	t_token	*history;
+	t_command	*commands;
+}	t_minishell;
+
+/* --------------------------------- minishell ------------------------------ */
+
+int		ft_atoi(const char *nptr);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+/* --------------------------------- builtin -------------------------------- */
+
+char	**get_env(void);
+char	*get_path(void);
+
+/* --------------------------------- builtin -------------------------------- */
+
+
+/* --------------------------------- minishell ------------------------------ */
 
 /* --------------------------------- parser --------------------------------- */
 /* --------------------------------- parser --------------------------------- */
+
+/* --------------------------------- exec ----------------------------------- */
+
+// void execute_command(t_command *cmd, minishell *shell);
+
+/* --------------------------------- exec ----------------------------------- */
 
 #endif
--- inc/parse.h
+++ inc/parse.h
@@ -1,11 +1,46 @@
 #ifndef PARSE_H
 # define PARSE_H
 
+# define MAX_ARGUMENTS 256
+
 # include "minishell.h"
 # include <string.h>
 # include <stdlib.h>
 
-char	**parse(char *user_input);
-int		ft_isspace(int c);
+typedef struct s_cmdline
+{
+	char				*arguments[MAX_ARGUMENTS];
+	int					argcount;
+	char const			*input_redirect;
+	char const			*output_redirect;
+	int					background;
+	struct s_cmdline	*next;
+}	t_cmdline;
+
+char		*parse(char *user_input);
+int			ft_isspace(int c);
+int			is_empty(const char *str);
+char		*str_clone(const char *source);
+t_cmdline	*parset_cmdlines(char* line);
 
 #endif
+
+/**
+ * qoutes or not (double and single)
+ * expansion $ ($PATH $HOME) get it from env
+ * 
+*/
+
+/**
+ * command
+ * flags
+ * piped or not
+*/
+
+/**
+ * $ " ' ! < >
+ * << (heredoc)
+ * >> < > redirections
+ *
+ * "   '$HOME' "
+*/
