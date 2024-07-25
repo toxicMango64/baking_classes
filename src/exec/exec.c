b2ffa0c0f67a6eda5dc295aee344aae151dc7e48
Your Name <you@example.com>
2024-07-25 12:13:16 +0400

    updated exec function

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -1,2 +1,16 @@
 #include "../../inc/exec.h"
 
+void execute_command(t_command *cmd, minishell *shell)
+{
+	// execute a single command
+	if (cmd->cmd == "echo")
+		// handle echo builtin
+	else if (cmd->cmd == "cd")
+		// handle cd builtin
+	else if (cmd->cmd == "cd")
+		// handle cd builtin
+	else if (cmd->cmd == "cd")
+		// handle cd builtin
+	else
+		// execute external command using execve
+}
